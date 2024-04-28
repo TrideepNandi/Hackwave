38369f6186afc20b1acdbd72dade6f1d68d45d27
vedant1711 <vscode1711@gmail.com>
2024-04-28 12:12:51 +0530

    added firebasemanager.py

---
+++ app/firebasemanager.py
@@ -0,0 +1,39 @@
+#firebasemanager.py
+
+# import firebase_admin
+# from firebase_admin import credentials, messaging
+
+# cred = credentials.Certificate("./serviceAccountKey.json")
+# firebase_admin.initialize_app(cred)
+
+
+# def sendPush(title, msg, registration_token, dataObject=None):
+#     # See documentation on defining a message payload.
+#     message = messaging.MulticastMessage(
+#         notification=messaging.Notification(
+#             title=title,
+#             body=msg
+#         ),
+#         data=dataObject,
+#         tokens=registration_token,
+#     )
+
+#     # Send a message to the device corresponding to the provided
+#     # registration token.
+#     response = messaging.send_multicast(message)
+#     # Response is a message ID string.
+#     print('Successfully sent message:', response)
+
+
+
+from pyfcm import FCMNotification
+
+def send_sos_ring(device_token):
+    push_service = FCMNotification(api_key="wVLYaJaKQOdXrzslTh7DMAkvR0DLbFeXpB0mFnEbmCA")
+
+    message_title = "SOS Alert"
+    message_body = "An elder has sent an SOS alert. Please check the app for details."
+
+    result = push_service.notify_single_device(registration_id=device_token, message_title=message_title, message_body=message_body)
+
+    return result
